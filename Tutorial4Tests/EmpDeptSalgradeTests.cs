7c3d26f40876864b15e96d25b01fdcbe7b9b7895
Gabriela <s28751@pjwstk.edu.pl>
2025-04-13 21:26:05 +0200

    Wykonanie testu 4

--- Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -1,4 +1,8 @@
 ﻿using Tutorial3.Models;
+namespace Tutorial3Tests;
+using Xunit;
+using Xunit.Abstractions;
+
 
 public class EmpDeptSalgradeTests
 {
@@ -66,13 +70,15 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        //var result = null; 
+        var result = emps
+            .Select(e => new {e.EName, e.Sal })
+            .ToList();
         
-        // Assert.All(result, r =>
-        // {
-        //     Assert.False(string.IsNullOrWhiteSpace(r.EName));
-        //     Assert.True(r.Sal > 0);
-        // });
+        Assert.All(result, r =>
+         {
+             Assert.False(string.IsNullOrWhiteSpace(r.EName));
+             Assert.True(r.Sal > 0);
+         });
     }
 
     // 5. JOIN Emp to Dept
